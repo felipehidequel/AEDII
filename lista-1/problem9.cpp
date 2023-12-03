53fc9e51fc1a94c23659568b9515c2090f2663e0
felipehidequel <felipehidequel@gmail.com>
2023-12-03 16:10:43 -0300

    added problem 9

---
+++ lista-1/problem9.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void to_ascii(int n = 0)
+{
+    if (n >= 127)
+        return;
+
+    cout << static_cast<char>(n) << " ";
+
+    if ((n+1) % 10 == 0)
+        cout << endl;
+
+    to_ascii(n + 1);
+}
+
+int main(){
+    to_ascii();
+    cout<<endl;
+    return 0;
+}
